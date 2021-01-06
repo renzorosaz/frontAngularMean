0f7ddebb4226030195d1f50fcd2cf1122dc09671
renzorosaz <renzo.rovi@gmail.com>
2021-01-05 22:30:17 -0500

    actualizando fron 2

---
+++ src/app/components/register/register.component.ts
@@ -0,0 +1,18 @@
+import {Component,OnInit} from '@angular/core';
+
+@Component({
+    selector: 'register',
+    templateUrl: './register.component.html'
+})
+
+export class RegisterComponent implements OnInit{
+    public title:string;
+
+    constructor(){
+        this.title ='Registrate';
+        
+    }
+    ngOnInit(){
+        console.log('Componente de register cargado...');
+    }
+}
